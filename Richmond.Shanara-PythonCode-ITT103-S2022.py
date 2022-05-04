3b3b9833e94373a5ecd2cc0b1d19291b100d4a52
Shanara-Richmond <104879565+Shanara-Richmond@users.noreply.github.com>
2022-05-03 23:16:06 -0500

    Add files via upload

---
+++ Richmond.Shanara-PythonCode-ITT103-S2022.py
@@ -0,0 +1,28 @@
+Python 3.11.0a7 (main, Apr  5 2022, 21:27:39) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+Name: "Shanara Richmond"
+Date_Created: "April 3, 2022"
+Course: "ITT103"
+Purpose: "To store employees information, and calculate and print employees name salary and commission"
+
+salesAmnt = Int
+Comm = float
+INPUT: "SalesPersonNum,SalesAmnt, C_lass1, C_lass2, C_lass3, RegRate"
+WRITE: "Class1 commission Rate is"
+READ: "Sales"
+if C_lass==1:
+    if salesAmnt<=1000:
+        comm=salesAmnt*0.06
+    elif salesAmnt>1000 and sales <2000:
+        comm=sales*0.07
+    else:
+if C_lass==2:
+    if sales<1000:
+        comm= salesAmnt*0.04
+    else:
+        comm=salesAmnt*0.06
+if C_lass==3:        comm=sales*0.1
+if C_lass==3:
+    comm=salesAmnt*0.045
+print ('your commission is',comm)
+print ("process completed. have a nice day!")
